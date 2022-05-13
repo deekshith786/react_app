dac1f8f9fa84ef23029cb31a29ef548ddc45f769
Deekshith <deekshith.m@comakeit.com>
2022-05-13 12:22:30 +0530

    added card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { EditPagecomponent } from './components/EditComponent/EditPage';
+import "./App.css";
+import { EditPagecomponent } from "./components/EditComponent/EditPage";
+import DisplayMarkets from "./components/DisplayMarket/DisplayMarketView";
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <EditPagecomponent/>
-    </div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<DisplayMarkets/>}>abc</Route>        
+        <Route path="/market" element={<DisplayMarkets/>}>abc</Route>
+        <Route path="/edit" element={<EditPagecomponent/>}>abc</Route>
+      </Routes>
+    </Router>
+
+
+
+
+     
   );
 }
 
--- src/components/EditComponent/EditPage.js
+++ src/components/EditComponent/EditPage.js
@@ -11,149 +11,98 @@ import {
   MenuItem,
   TextField,
 } from "@mui/material";
+import { display, padding } from "@mui/system";
+import { useEffect } from "react";
+import { useState } from "react";
+import './EditPage.css'
 
 export const EditPagecomponent = (params) => {
+
+  const [MarketData,setMarketData] = useState([]);
+
+  const getAllData = () =>{
+
+  }
+
+  useEffect(()=>{
+    //for worldwide fetch
+      fetch("http://localhost:9000/market/getMarketByName/Abc")
+      .then(response => response.json())
+      .then((data)=>{
+          setMarketData(data);
+          console.log(data);
+
+      });
+    },[]);
+
   return (
     <div>
-      <h1>Edit</h1>
-      <hr />
-      <h3>Market</h3>
-      <h3>Photp</h3>
-      <Button variant="contained">Contained</Button>
-      <p>No file choosen</p>
+      <div className="editHeader">
+        <h1>Edit</h1><hr/>
+        <p style={{display:'block'}}><b>Market</b></p>
+        <p style={{display:'block'}}><b>Photo</b></p>
+        <Button className="FileButton" variant="contained">choose file</Button>
+        <p>No file choosen</p>
+      </div>
       <div>
         <Card>
           <CardContent>
             <Grid container spacing={1}></Grid>
             <Grid xs={12} sm={6} item>
+            <h3>TimeZone</h3>
+            <TextField fullWidth select label="TimeZone" ></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Name</h3>
               <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
             </Grid>
             <Grid xs={12} sm={6} item>
+            <h3>State</h3>
               <TextField fullWidth label="Name" placeholder="Name" variant="outlined"></TextField>
             </Grid>
             <Grid xs={12} sm={6} item>
+            <h3>Longitude</h3>
               <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
             </Grid>
             <Grid xs={12} sm={6} item>
+            <h3>Latitude</h3>
               <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
             </Grid>
             <Grid xs={12} sm={6} item>
+            <h3>Map Zoom</h3>
               <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
             </Grid>
             <Grid xs={12} sm={6} item>
+            <h3>Android Map Zoom</h3>
               <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
             </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Ios Map Zoom</h3>
+              <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Default Home screen</h3>
+              <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Reservation maximum duration</h3>
+              <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Validation account reservation maximum duration</h3>
+              <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <h3>Description</h3>
+              <TextField fullWidth label="Select" placeholder="Select" variant="outlined"></TextField>
+            </Grid><br/>
+            
+            <Button variant="contained" onClick={getAllData()}>Edit</Button>
+
           </CardContent>
         </Card>
-        <h4>TimeZone</h4>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          helperText="Please select your currency"
-        ></TextField>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Name"
-          multiline
-          maxRows={4}
-          helperText="Please select your currency"
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="State"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Latitude"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Longitude"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Map Zoom"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-        />
-      </div>
-      <FormControl>
-        <FormLabel>
-          <b>Name</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>State</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Latitute</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Longitude</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Map zoom</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Android map zoom</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>ios map zoom</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Default home screen</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Reservation maximum duration(in days)</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Validation account reservation maximum duration</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormLabel>
-          <b>Description</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-      </FormControl>
-      <FormControl>
-        <FormLabel>
-          <b>Description</b>
-        </FormLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-      </FormControl>
+     </div>
     </div>
+
   );
 };
